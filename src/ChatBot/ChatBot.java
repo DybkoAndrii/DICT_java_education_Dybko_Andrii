7226b52164877b6393f331556b8633ff25bde3e7
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-10-17 19:33:54 +0300

    ChatBot 3 stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -9,5 +9,12 @@ public class ChatBot {
         Scanner userInput = new Scanner(System.in);
         String Name = userInput.nextLine();
         System.out.println("What a great name you have, " + Name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int rm3 = userInput.nextInt();
+        int rm5 = userInput.nextInt();
+        int rm7 = userInput.nextInt();
+        int age = (rm3 * 70 + rm5 * 21 + rm7 * 15) % 105;
+        System.out.println("Your age is " + age +"; that`s a good time to start programming!");
     }
 }
