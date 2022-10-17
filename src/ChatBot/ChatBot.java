df762efd952560f50b631f91a8e0818f846a664e
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-10-17 18:38:49 +0300

    ChatBot 1 stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.print("Hello! My name is Chmon`a.\n" +
+                "I was created in 2022.");
+    }
+}
