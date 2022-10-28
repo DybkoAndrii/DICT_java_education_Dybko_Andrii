8c7138ae493bb8503e51139985c33cf6149a71de
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-10-29 00:05:16 +0300

    Hangman 5 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -6,22 +6,30 @@ public class hangman {
     public static void main(String[] args){
         System.out.println("HANGMAN");
         String[] words = new String[]{"python", "java", "javascript", "kotlin"};
-        int n = (int)Math.floor(Math.random() * words.length);
+        int n = (int)(Math.random() * words.length);
         char[] chArray = words[n].toCharArray();
-        System.out.print("Guess the word ");
+        char[] userChArray = new char[chArray.length];
         for (int i = 0; i < chArray.length; i++){
-            if (i < 2){
-                System.out.print(chArray[i]);
-            }else {
-                System.out.print('-');
+            userChArray[i] = '-';
+        }
+        Scanner userInput = new Scanner(System.in);
+        for (int j = 0; j < 8; j++) {
+            for (int i = 0; i < chArray.length; i++) {
+                System.out.print(userChArray[i]);
+            }
+            System.out.println(' ');
+            System.out.print("Input a letter: ");
+            char userChar = userInput.nextLine().toCharArray()[0];
+            int b = 0;
+            for (int i = 0; i < chArray.length; i++){
+                if (chArray[i] == userChar){
+                    userChArray[i] = userChar;
+                    b++;
+                }
+            } if (b == 0){
+                System.out.println("That letter doesn't appear in the word");
             }
         }
-        System.out.print(": ");
-        Scanner userInput = new Scanner(System.in);
-        if (userInput.nextLine().equals(words[n])){
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
-        }
+        System.out.println("Thanks for playing!\nWe'll see how well you did in the next stage");
     }
 }
