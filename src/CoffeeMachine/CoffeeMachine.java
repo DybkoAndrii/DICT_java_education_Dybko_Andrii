3f79a8620fe65831dc38b92fb37f0d1f11af4a71
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-11-01 00:32:07 +0200

    CoffeeMachine 4 stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,25 +1,68 @@
 package CoffeeMachine;
 
-public class CoffeeMachine {
-    private final int water;
-    private final int milk;
-    private final int coffee;
-    private final int cups;
+import java.util.Scanner;
 
-    public CoffeeMachine(int cups, int water, int milk, int coffee){
+public class CoffeeMachine {
+    private int water, milk, coffee, cups, money;
+
+    public CoffeeMachine(int cups, int water, int milk, int coffee, int money){
         this.cups = cups;
         this.water = water;
         this.milk = milk;
         this.coffee = coffee;
+        this.money = money;
+    }
+
+    public void espresso(){
+        cups -= 1;
+        water -= 250;
+        money += 4;
+    }
+
+    public void latte(){
+        cups -= 1;
+        water -= 350;
+        milk -= 75;
+        coffee -= 20;
+        money += 7;
+    }
+
+    public void cappuccino(){
+        cups -= 1;
+        water -= 200;
+        milk -= 100;
+        coffee -= 12;
+        money += 6;
+    }
+
+    public void fill(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write how many ml of water you want to add:");
+        water += input.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        milk += input.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        coffee += input.nextInt();
+        System.out.println("Write how many disposable coffee cups you want to add:");
+        cups += input.nextInt();
+    }
+
+    public void take(){
+        System.out.printf("""
+                I gave you %d
+                
+                """, money);
+        money = 0;
     }
 
     public void info(){
-        int num = (int) Math.floor(Math.min(water/200, Math.min(milk/50, coffee/15)));
-        if (num == cups)
-            System.out.println("Yes, I can make that amount of coffee");
-        else if (num > cups) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", num-cups);
-        } else
-            System.out.printf("No, I can make only %d cups of coffee", num);
+        System.out.printf("""
+                The coffee machine has:
+                %d of water
+                %d of milk
+                %d of coffee beans
+                %d of disposable cups
+                %d of money
+                """, water, milk, coffee, cups, money);
     }
 }
--- src/CoffeeMachine/Main.java
+++ src/CoffeeMachine/Main.java
@@ -3,17 +3,30 @@ package CoffeeMachine;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = input.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = input.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffee = input.nextInt();
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = input.nextInt();
-        CoffeeMachine machine = new CoffeeMachine(cups, water, milk, coffee);
+        CoffeeMachine machine = new CoffeeMachine(9, 400, 540, 120, 550);
         machine.info();
+        System.out.println("Write action (buy, fill, take): ");
+        String answer = input.nextLine();
+        switch (answer) {
+            case "buy" -> buy(machine);
+            case "fill" -> machine.fill();
+            case "take" -> machine.take();
+            default -> System.out.println("ERROR");
+        }
+        machine.info();
+    }
+
+    private static void buy(CoffeeMachine machine){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+        String answer = input.nextLine();
+        switch (answer) {
+            case "1" -> machine.espresso();
+            case "2" -> machine.latte();
+            case "3" -> machine.cappuccino();
+            default -> System.out.println("ERROR");
+        }
     }
 }
