e1d800f5cd8f0c4d929f50086661d459da401a5b
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-11-01 01:55:16 +0200

    CoffeeMachine 6 stage

--- src/CoffeeMachine/Main.java
+++ src/CoffeeMachine/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         CoffeeMachine machine = new CoffeeMachine(9, 400, 540, 120, 550);
-        System.out.println("Write action (buy, fill, take, remaining, exit): ");
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
         String answer = input.nextLine();
         while (!answer.equals("exit")){
             machine.menu(answer);
-            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
             answer = input.nextLine();
         }
     }
