c8c4e8773546bf78adffa42eafa1485d2b500817
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-11-01 01:54:30 +0200

    CoffeeMachine 5 stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class CoffeeMachine {
     private int water, milk, coffee, cups, money;
 
+    public void menu(String answer){
+        switch (answer) {
+            case "buy" -> buy();
+            case "fill" -> fill();
+            case "take" -> take();
+            case "remaining" -> info();
+            default -> System.out.println("ERROR");
+        }
+    }
+
+    private void buy(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu");
+        String answer = input.nextLine();
+        switch (answer) {
+            case "1" -> espresso();
+            case "2" -> latte();
+            case "3" -> cappuccino();
+            case "back" -> System.out.println(" ");
+            default -> System.out.println("ERROR");
+        }
+    }
+
     public CoffeeMachine(int cups, int water, int milk, int coffee, int money){
         this.cups = cups;
         this.water = water;
@@ -13,29 +36,42 @@ public class CoffeeMachine {
         this.money = money;
     }
 
-    public void espresso(){
-        cups -= 1;
-        water -= 250;
-        money += 4;
+    private void espresso(){
+        if (make_coffee(250, 0, 16).equals("has")){
+            cups -= 1;
+            water -= 250;
+            coffee -= 16;
+            money += 4;
+            System.out.println("I have enough resources, making you a espresso!");
+        }else
+            System.out.printf("%s\n", make_coffee(250, 0, 16));
     }
 
-    public void latte(){
-        cups -= 1;
-        water -= 350;
-        milk -= 75;
-        coffee -= 20;
-        money += 7;
+    private void latte(){
+        if (make_coffee(350, 75, 20).equals("has")) {
+            cups -= 1;
+            water -= 350;
+            milk -= 75;
+            coffee -= 20;
+            money += 7;
+            System.out.println("I have enough resources, making you a latte!");
+        }else
+            System.out.printf("%s\n", make_coffee(350, 75, 20));
     }
 
-    public void cappuccino(){
-        cups -= 1;
-        water -= 200;
-        milk -= 100;
-        coffee -= 12;
-        money += 6;
+    private void cappuccino(){
+        if (make_coffee(200, 100, 12).equals("has")) {
+            cups -= 1;
+            water -= 200;
+            milk -= 100;
+            coffee -= 12;
+            money += 6;
+            System.out.println("I have enough resources, making you a cappuccino!");
+        }else
+            System.out.printf("%s\n", make_coffee(200, 100, 12));
     }
 
-    public void fill(){
+    private void fill(){
         Scanner input = new Scanner(System.in);
         System.out.println("Write how many ml of water you want to add:");
         water += input.nextInt();
@@ -47,7 +83,7 @@ public class CoffeeMachine {
         cups += input.nextInt();
     }
 
-    public void take(){
+    private void take(){
         System.out.printf("""
                 I gave you %d
                 
@@ -55,7 +91,20 @@ public class CoffeeMachine {
         money = 0;
     }
 
-    public void info(){
+    private String make_coffee(int wt, int mlk, int cfe){
+        String isHasSours = "has";
+        if (water < wt)
+            isHasSours = "Sorry, not enough water!";
+        else if (milk < mlk) {
+            isHasSours = "Sorry, not enough milk!";
+        } else if (coffee < cfe) {
+            isHasSours = "Sorry, not enough coffee beans!";
+        } else if (cups - 1 < 0) {
+            isHasSours = "Sorry, not enough disposable cups!";
+        } return isHasSours;
+    }
+
+    private void info(){
         System.out.printf("""
                 The coffee machine has:
                 %d of water
--- src/CoffeeMachine/Main.java
+++ src/CoffeeMachine/Main.java
@@ -6,27 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         CoffeeMachine machine = new CoffeeMachine(9, 400, 540, 120, 550);
-        machine.info();
-        System.out.println("Write action (buy, fill, take): ");
+        System.out.println("Write action (buy, fill, take, remaining, exit): ");
         String answer = input.nextLine();
-        switch (answer) {
-            case "buy" -> buy(machine);
-            case "fill" -> machine.fill();
-            case "take" -> machine.take();
-            default -> System.out.println("ERROR");
-        }
-        machine.info();
-    }
-
-    private static void buy(CoffeeMachine machine){
-        Scanner input = new Scanner(System.in);
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        String answer = input.nextLine();
-        switch (answer) {
-            case "1" -> machine.espresso();
-            case "2" -> machine.latte();
-            case "3" -> machine.cappuccino();
-            default -> System.out.println("ERROR");
+        while (!answer.equals("exit")){
+            machine.menu(answer);
+            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            answer = input.nextLine();
         }
     }
 }
